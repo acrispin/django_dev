3aa45094ec743b925bc2d7f9576d7a623e866113
acrispin <antonio.crispin@gmail.com>
2015-08-30 04:03:08 -0500

    updated

--- basedev/wsgi_dev.py
+++ basedev/wsgi_dev.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_test project.
+WSGI config for basedev project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basedev.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
